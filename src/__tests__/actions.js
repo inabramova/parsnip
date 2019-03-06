04c0fac08afbf31a2261aa8be7b55998eeea2e2c
Irina Abramova <inabramova@gmail.com>
2019-03-05 18:49:43 -0800

    rename file

--- src/__tests__/actions.js
+++ src/__tests__/actions.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { createTask } from './index';
+import { createTask } from '../actions/index';
 import * as api from '../api';
 
 jest.unmock('../api');
